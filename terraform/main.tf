a41683c62262cea8e40a927724c4b5ee515eac13
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-02-12 11:56:32 +0530

    Update AWS instance and security group configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "game_server" {
   ami           = "ami-019715e0d74f695be" 
-  instance_type = "t2.small"
-  key_name      = "jenkins"
+  instance_type = "t3.small"
+  key_name      = "new"
 
-  security_groups = [aws_security_group.myDevopsProj.name]
+  security_groups = [aws_security_group.game.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -22,8 +22,8 @@ resource "aws_instance" "game_server" {
   }
 }
 
-resource "aws_security_group" "myDevopsProj" {
-  name = "myDevopsProj"
+resource "aws_security_group" "game" {
+  name = "game"
 
   ingress {
     from_port   = 22
